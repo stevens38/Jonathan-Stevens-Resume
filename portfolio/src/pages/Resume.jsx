d478ac3c3de62f17f72241f4d7331202a4858717
JStevens <stevens38@marshall.edu>
2024-11-06 21:15:19 -0500

    resume.jsx page updated

---
+++ portfolio/src/pages/Resume.jsx
@@ -0,0 +1,46 @@
+export default function Resume() {
+    return (
+        <div className="Resume">
+        <h1>Jonathan Stevens</h1>
+        <p> 
+            <strong>Email:</strong>
+            <a href="mailto:jonathan.stevens@example.com">jonathan.stevens@example.com</a>
+            <strong>Phone:</strong>
+            <a href="tel:1-304-617-7563">1-304-617-7563</a>
+        </p>
+        <h2>Education</h2>
+        <p>
+            <strong>Marshall University of West Virginia, Huntington</strong>
+            <br />
+            Bachelor of Science in Computer Science
+            <br />
+            Graduated June 2009
+        </p>
+        <h2>Experience</h2>
+        <p>
+            <strong>Software Engineer</strong>
+            <br />
+            <strong>Company:</strong> ACME Corporation
+            <br />
+            <strong>Location:</strong> Huntington, WV
+            <br />
+            <strong>Duration:</strong> June 2010 - Present
+            <br />
+            <strong>Responsibilities:</strong>
+            <ul>
+                <li>Developed software applications for various platforms</li>
+                <li>Worked with a team of developers to meet project deadlines</li>
+                <li>Collaborated with clients to determine project requirements</li>
+            </ul>
+        </p>
+        <h2>Skills</h2>
+        <ul>
+            <li>Java</li>
+            <li>Python</li>
+            <li>JavaScript</li>
+            <li>HTML</li>
+            <li>CSS</li>
+        </ul>
+        </div>
+    );
+}
