2d0b95116b4330c2482cbe401b0e184aa1c81056
JStevens <stevens38@marshall.edu>
2024-11-06 20:41:43 -0500

    main.jsx update

--- portfolio/src/main.jsx
+++ portfolio/src/main.jsx
@@ -1,7 +1,43 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
 import App from './App.jsx'
+
+import ErrorPage from './ErrorPage.jsx'
+import AboutMe from './aboutMe.jsx'
+import ResumePage from './ResumePage.jsx'
+import ProjectsPage from './ProjectsPage.jsx'
+import ContactPage from './ContactPage.jsx'
+import React from 'react';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <AboutMe />,
+      },
+      {
+        path: '/resume',
+        element: <ResumePage />,
+      },
+      {
+        path: '/projects',
+        element: <ProjectsPage />,
+      },
+      {
+        path: '/contact',
+        element: <ContactPage />,
+      },
+    ],
+  },
+])
+    
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
