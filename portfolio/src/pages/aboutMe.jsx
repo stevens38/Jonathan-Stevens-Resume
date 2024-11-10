8ace59ea6e1850b442e50be3720bc06865207eb2
JStevens <stevens38@marshall.edu>
2024-11-10 18:27:52 -0500

    AboutMe.jsx updated

---
+++ portfolio/src/pages/aboutMe.jsx
@@ -0,0 +1,10 @@
+export default function AboutMe() {
+    return (
+        <div>
+        <h1>About Me</h1>
+        <p>
+            I am a software engineer with over 10 years of experience in developing software applications for various platforms. I have worked with a team of developers to meet project deadlines and collaborated with clients to determine project requirements. I am proficient in Java, Python, JavaScript, HTML, and CSS.
+        </p>
+        </div>
+    );
+}
