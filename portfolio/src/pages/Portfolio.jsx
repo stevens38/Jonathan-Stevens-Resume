a2d5b04adee5325278e6458416cf416ab3514924
JStevens <stevens38@marshall.edu>
2024-11-10 18:38:05 -0500

    portfolio updated

---
+++ portfolio/src/pages/Portfolio.jsx
@@ -0,0 +1,12 @@
+
+
+export default function Portfolio() {
+    return (
+        <div>
+        <h1>Portfolio</h1>
+        <p> 
+            <strong>Email:</strong>
+        </p>
+        </div>
+    );
+}
