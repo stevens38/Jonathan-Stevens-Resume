ab72be980f95309253d9055b8b1de83b77887a67
JStevens <stevens38@marshall.edu>
2024-11-10 18:31:57 -0500

    contact.jsx updated

---
+++ portfolio/src/pages/contact.jsx
@@ -0,0 +1,15 @@
+
+export default function contact() {
+    return (
+        <div>
+        <h1>Contact Me</h1>
+        <p>Feel free to reach out to me at stevens38@marshall.edu or fill out the form below.</p>
+        <form>
+          <input type="text" placeholder="Name" />
+          <input type="email" placeholder="Email" />
+          <textarea placeholder="Message" />
+          <button>Submit</button>
+        </form>
+      </div>
+    );
+}
