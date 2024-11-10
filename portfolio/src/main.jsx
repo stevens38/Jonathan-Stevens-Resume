1771c41a80222673535c6653af9a023ee1339f8d
JStevens <stevens38@marshall.edu>
2024-11-10 18:18:30 -0500

    App.jsx and main.jsx update

--- portfolio/src/main.jsx
+++ portfolio/src/main.jsx
@@ -1,5 +1,4 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client';
 import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
@@ -39,8 +38,6 @@ const router = createBrowserRouter([
 ])
     
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDom.createRoot(document.getElementById('root')).render(
+ <RouterProvider router={router} />
 )
